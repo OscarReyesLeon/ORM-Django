fc4ac403c1bac1a827caf299452ac94e9e692f22
Oscar Reyes <oscar@mercax.com>
2021-10-10 22:04:42 -0500

    OneToOneField

--- app1/models.py
+++ app1/models.py
@@ -104,3 +104,12 @@ class Libro(ModeloAuditoria):
     class Meta:
         verbose_name_plural = "libros",
         unique_together = ('nombre','tipo')
+
+class Progenitor(ModeloAuditoria):
+    persona = models.OneToOneField(Persona,on_delete=models.CASCADE)
+    padre = models.CharField(max_length=50)
+    madre = models.CharField(max_length=50)
+    def __str__(self):
+        return "{} - {} - {}".format(self.persona,self.madre,self.padre)
+    class Meta:
+        verbose_name = "Progenitores"
