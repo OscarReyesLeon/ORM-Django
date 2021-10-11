3d8a4c340bf6153642f6eb53bf031e94d1e9e0e6
Oscar Reyes <oscar@mercax.com>
2021-10-10 22:44:50 -0500

    ManyToManyField

--- app1/models.py
+++ app1/models.py
@@ -127,4 +127,16 @@ class Hijo(models.Model):
 
     def __str__(self):
         return "{} hijo de {}".format(self.nombre,self.padre)
-        
+
+class Publicacion(models.Model):
+    titulo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.titulo
+
+class Articulo(models.Model):
+    titular = CharField(max_length=100)
+    publicacion = models.ManyToManyField(Publicacion)
+
+    def __str__(self):
+        return self.titular
